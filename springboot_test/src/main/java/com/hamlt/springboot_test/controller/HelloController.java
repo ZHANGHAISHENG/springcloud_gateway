3571f9b9d58b75de2a48a800c17ed0701a8bebaa
zhanghaisheng <332627460@qq.com>
2020-05-26 00:54:41 +0800

    xx

---
+++ springboot_test/src/main/java/com/hamlt/springboot_test/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.hamlt.springboot_test.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ * @date 2020-05-25 23:55
+ **/
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
